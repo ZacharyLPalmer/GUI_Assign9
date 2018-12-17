bc5387e4ffc4ba0cef6b2b6d51c9ec4370f06a6e
Zachary Palmer <zanyzakary@gmail.com>
2018-12-17 12:08:05 -0500

    Add files via upload
    final JS file

--- js/scrabble.js
+++ js/scrabble.js
@@ -1,46 +1,62 @@
 /* Name: Zachary Palmer
-Date: 12/4/18
-Assignment: 8
+Date: 12/19/18
+Assignment: 9
 Class: GUI 1
 */
 
+var remainingTiles = 100; // remaining tile count
+var currentScore = 0;     // current score
+var currentWordScore = 0; // current word score
+var currentWord = "";     // current word
 
-function validateWord() {
-  var i, e;
-  var word = "";
-  for (i = 1; i < 16; i++) {
-    for (e = 1; e < 16; e++) {
-      var letter = $("#line" + i).find("#space" + e).find(".game_tile").attr("letter");
-      if (letter == undefined) {
-        letter = " ";
-      }
-      word += letter;
-    }
-  }
-  word = $.trim(word);
-  if (word.includes(" ")) {
-    return false;
-  }
-  return true;
-}
+var ScrabbleTiles = []; // associative array of our bag of letters
+ScrabbleTiles["A"] = { "value": 1, "original-distribution": 9, "number-remaining": 9, "image": "media/A.jpg" };
+ScrabbleTiles["B"] = { "value": 3, "original-distribution": 2, "number-remaining": 2, "image": "media/B.jpg" };
+ScrabbleTiles["C"] = { "value": 3, "original-distribution": 2, "number-remaining": 2, "image": "media/C.jpg" };
+ScrabbleTiles["D"] = { "value": 2, "original-distribution": 4, "number-remaining": 4, "image": "media/D.jpg" };
+ScrabbleTiles["E"] = { "value": 1, "original-distribution": 12, "number-remaining": 12, "image": "media/E.jpg" };
+ScrabbleTiles["F"] = { "value": 4, "original-distribution": 2, "number-remaining": 2, "image": "media/F.jpg" };
+ScrabbleTiles["G"] = { "value": 2, "original-distribution": 3, "number-remaining": 3, "image": "media/G.jpg" };
+ScrabbleTiles["H"] = { "value": 4, "original-distribution": 2, "number-remaining": 2, "image": "media/H.jpg" };
+ScrabbleTiles["I"] = { "value": 1, "original-distribution": 9, "number-remaining": 9, "image": "media/I.jpg" };
+ScrabbleTiles["J"] = { "value": 8, "original-distribution": 1, "number-remaining": 1, "image": "media/J.jpg" };
+ScrabbleTiles["K"] = { "value": 5, "original-distribution": 1, "number-remaining": 1, "image": "media/K.jpg" };
+ScrabbleTiles["L"] = { "value": 1, "original-distribution": 4, "number-remaining": 4, "image": "media/L.jpg" };
+ScrabbleTiles["M"] = { "value": 3, "original-distribution": 2, "number-remaining": 2, "image": "media/M.jpg" };
+ScrabbleTiles["N"] = { "value": 1, "original-distribution": 6, "number-remaining": 6, "image": "media/N.jpg" };
+ScrabbleTiles["O"] = { "value": 1, "original-distribution": 8, "number-remaining": 8, "image": "media/O.jpg" };
+ScrabbleTiles["P"] = { "value": 3, "original-distribution": 2, "number-remaining": 2, "image": "media/P.jpg" };
+ScrabbleTiles["Q"] = { "value": 10, "original-distribution": 1, "number-remaining": 1, "image": "media/Q.jpg" };
+ScrabbleTiles["R"] = { "value": 1, "original-distribution": 6, "number-remaining": 6, "image": "media/R.jpg" };
+ScrabbleTiles["S"] = { "value": 1, "original-distribution": 4, "number-remaining": 4, "image": "media/S.jpg" };
+ScrabbleTiles["T"] = { "value": 1, "original-distribution": 6, "number-remaining": 6, "image": "media/T.jpg" };
+ScrabbleTiles["U"] = { "value": 1, "original-distribution": 4, "number-remaining": 4, "image": "media/U.jpg" };
+ScrabbleTiles["V"] = { "value": 4, "original-distribution": 2, "number-remaining": 2, "image": "media/V.jpg" };
+ScrabbleTiles["W"] = { "value": 4, "original-distribution": 2, "number-remaining": 2, "image": "media/W.jpg" };
+ScrabbleTiles["X"] = { "value": 8, "original-distribution": 1, "number-remaining": 1, "image": "media/X.jpg" };
+ScrabbleTiles["Y"] = { "value": 4, "original-distribution": 2, "number-remaining": 2, "image": "media/Y.jpg" };
+ScrabbleTiles["Z"] = { "value": 10, "original-distribution": 1, "number-remaining": 1, "image": "media/Z.jpg" };
+ScrabbleTiles["_"] = { "value": 0, "original-distribution": 2, "number-remaining": 2, "image": "media/Blank.jpg" };
+
+
 
  // updates the current word value and score at the bottom of the scren
 function updateCurrentWord() {
   var i, e; // index variables
   var hWord = ""; // horizontal word temp value
   var vWord = ""; // vertical word temp value
-  var multH = 1;
+  var multH = 1; //multiplier values for horizontal and veritical words
   var multV = 1;
-  var tempLScore = 0;
-  var tempHWScore = 0;
+  var tempLScore = 0; //temporary lett score
+  var tempHWScore = 0; // temporary scores for  horizontal and veritical words
   var tempVWScore = 0;
-  for (i = 1; i < 16; i++) {
-    for (e = 1; e < 16; e++) {
-      // build both vertical and horizontal words at the same time, insert spaces where
-      // empty blocks are
+  for (i = 1; i < 16; i++) { // increments by line
+    for (e = 1; e < 16; e++) { //increments by space
+      // retrieve letter value from tile
       var lh = $("#line" + i).find("#space" + e).find(".game_tile").attr("letter");
       var lv = $("#line" + e).find("#space" + i).find(".game_tile").attr("letter");
       if (lh == undefined) {
+        // set to a space if no tile is on space
         lh = " ";
       } else { //scoring
         //sets our temporary letter score to the value of the letter
@@ -82,9 +98,13 @@ function updateCurrentWord() {
         // add final letter score to word score
         tempVWScore += tempLScore;
       }
+      // add letter to temp words
       hWord += lh;
       vWord += lv;
     }
+    // add space at the end of each line to ensure no overflow
+    hWord += " ";
+    vWord += " ";
   }
   // trim the white space from the front and back of both words
   hWord = $.trim(hWord);
@@ -92,7 +112,7 @@ function updateCurrentWord() {
   // if horizontal word does not include any spaces and it is 2+ characters long
   // then it is the current word
   if(!hWord.includes(" ") && hWord.length > 1) {
-    $("#currentWord").html("Word:" + hWord);
+    $("#currentWord").html("Word: " + hWord);
     $("#currentWord2").html("Word Score: 0" + multH * tempHWScore);
     currentWord = hWord;
     currentWordScore = multH * tempHWScore;
@@ -100,7 +120,7 @@ function updateCurrentWord() {
   // if vertical word does not include any spaces and it is 2+ characters long
   // then it is the current word
   if(!vWord.includes(" ") && vWord.length > 1) {
-    $("#currentWord").html("Word:" + vWord);
+    $("#currentWord").html("Word: " + vWord);
     $("#currentWord2").html("Word Score: 0" + multV * tempVWScore);
     currentWord = vWord;
     currentWordScore = multV * tempVWScore;
@@ -109,65 +129,42 @@ function updateCurrentWord() {
     $("#currentWord").html("Word: N/a");
     $("#currentWord2").html("Word Score: 0");
     currentWord = "";
-    currentWordScore = -1;
+    currentWordScore = 0;
   }
 }
 
+
 $(function () {
+  // make game tiles draggable with revert if placed incorrectly
   $(".game_tile").draggable({
     snap: ".board_tile,.game_tile_holder",
     snapmode: "both",
     revert: "invalid"
   });
 
+  // make the tile holder droppable so you can drag a tile from the board
+
   $(".game_tile_holder").droppable({
     drop: function (event, ui) {
-      ui.draggable.appendTo(this).css("left", "0px").css("top", "0px")
-      updateCurrentWord();
+      // make sure there isn't already a tile on this space
+      if($(this).children().length == 0){
+        ui.draggable.appendTo(this).css("left", "0px").css("top", "0px")
+        updateCurrentWord();
+      }
     }
   });
 
+  // make board tiles droppable so you can drag a tile from the holder
   $(".board_tile").droppable({
     drop: function (event, ui) {
-      ui.draggable.appendTo(this).css("left", "0px").css("top", "0px")
-      updateCurrentWord();
+      // make sure there isn't already a tile on this space
+      if($(this).children().length == 0){
+        ui.draggable.appendTo(this).css("left", "0px").css("top", "0px")
+        updateCurrentWord();
+      }
     }
   });
 });
-
-var ScrabbleTiles = [];
-ScrabbleTiles["A"] = { "value": 1, "original-distribution": 9, "number-remaining": 9, "image": "media/A.jpg" };
-ScrabbleTiles["B"] = { "value": 3, "original-distribution": 2, "number-remaining": 2, "image": "media/B.jpg" };
-ScrabbleTiles["C"] = { "value": 3, "original-distribution": 2, "number-remaining": 2, "image": "media/C.jpg" };
-ScrabbleTiles["D"] = { "value": 2, "original-distribution": 4, "number-remaining": 4, "image": "media/D.jpg" };
-ScrabbleTiles["E"] = { "value": 1, "original-distribution": 12, "number-remaining": 12, "image": "media/E.jpg" };
-ScrabbleTiles["F"] = { "value": 4, "original-distribution": 2, "number-remaining": 2, "image": "media/F.jpg" };
-ScrabbleTiles["G"] = { "value": 2, "original-distribution": 3, "number-remaining": 3, "image": "media/G.jpg" };
-ScrabbleTiles["H"] = { "value": 4, "original-distribution": 2, "number-remaining": 2, "image": "media/H.jpg" };
-ScrabbleTiles["I"] = { "value": 1, "original-distribution": 9, "number-remaining": 9, "image": "media/I.jpg" };
-ScrabbleTiles["J"] = { "value": 8, "original-distribution": 1, "number-remaining": 1, "image": "media/J.jpg" };
-ScrabbleTiles["K"] = { "value": 5, "original-distribution": 1, "number-remaining": 1, "image": "media/K.jpg" };
-ScrabbleTiles["L"] = { "value": 1, "original-distribution": 4, "number-remaining": 4, "image": "media/L.jpg" };
-ScrabbleTiles["M"] = { "value": 3, "original-distribution": 2, "number-remaining": 2, "image": "media/M.jpg" };
-ScrabbleTiles["N"] = { "value": 1, "original-distribution": 6, "number-remaining": 6, "image": "media/N.jpg" };
-ScrabbleTiles["O"] = { "value": 1, "original-distribution": 8, "number-remaining": 8, "image": "media/O.jpg" };
-ScrabbleTiles["P"] = { "value": 3, "original-distribution": 2, "number-remaining": 2, "image": "media/P.jpg" };
-ScrabbleTiles["Q"] = { "value": 10, "original-distribution": 1, "number-remaining": 1, "image": "media/Q.jpg" };
-ScrabbleTiles["R"] = { "value": 1, "original-distribution": 6, "number-remaining": 6, "image": "media/R.jpg" };
-ScrabbleTiles["S"] = { "value": 1, "original-distribution": 4, "number-remaining": 4, "image": "media/S.jpg" };
-ScrabbleTiles["T"] = { "value": 1, "original-distribution": 6, "number-remaining": 6, "image": "media/T.jpg" };
-ScrabbleTiles["U"] = { "value": 1, "original-distribution": 4, "number-remaining": 4, "image": "media/U.jpg" };
-ScrabbleTiles["V"] = { "value": 4, "original-distribution": 2, "number-remaining": 2, "image": "media/V.jpg" };
-ScrabbleTiles["W"] = { "value": 4, "original-distribution": 2, "number-remaining": 2, "image": "media/W.jpg" };
-ScrabbleTiles["X"] = { "value": 8, "original-distribution": 1, "number-remaining": 1, "image": "media/X.jpg" };
-ScrabbleTiles["Y"] = { "value": 4, "original-distribution": 2, "number-remaining": 2, "image": "media/Y.jpg" };
-ScrabbleTiles["Z"] = { "value": 10, "original-distribution": 1, "number-remaining": 1, "image": "media/Z.jpg" };
-ScrabbleTiles["_"] = { "value": 0, "original-distribution": 2, "number-remaining": 2, "image": "media/Blank.jpg" };
-
-var remainingTiles = 100;
-var currentScore = 0;
-var currentWordScore = 0;
-var currentWord;
 
 // Takes in a tile parameter as an index of the 7 spots in the tile holder 
 // Generates a new tile for that spot based on the probability of the tile
@@ -204,88 +201,75 @@ function refreshTile(tile) {
   }
 }
 
-
-
-function updateScore() { //function to update the score
-  var mult = 1;       // multiplaction value for the word, defaults to 1
-  var tempWScore = 0;  // temporary value for storing word score
-  var tempLScore = 0; // temporary value for storing word score
-  var i;              // index var
-  for (i = 1; i < 16; i++) {
-    for (e = 1; e < 16; e++) {
-      //retrieve letter value from space
-      var letter = $("#line" + i).find("#space" + e).find(".game_tile").attr("letter");
-      //check is letter is empty
-      if (letter != undefined) {
-        //sets our temporary letter score to the value of the letter
-        tempLScore = ScrabbleTiles[letter]["value"];
-        //triple word
-        if ($("#line" + i).find("#space" + e).attr("effect") == "tw") {
-          mult = 3;
-        } else //double word
-        if ($("#line" + i).find("#space" + e).attr("effect") == "dw") {
-            mult = 2;
-        } else //triple letter
-        if ($("#line" + i).find("#space" + e).attr("effect") == "tl") {
-              tempLScore = 3 * tempLScore;
-        } else //double letter
-        if ($("#line" + i).find("#space" + e).attr("effect") == "dl") {
-          tempLScore = 2 * tempLScore;
-        }
-        // add final letter score to word score
-        tempWScore += tempLScore;
-      }
-    }
-  }
-  //add final word score to current score
-  currentScore += (mult * tempWScore);
-  //update output
-  $("#currentScore").html("Score: " + currentScore)
-}
-
-function clearLine() {
+// clears board of tiles
+function clearBoard() {
   var i; //index variable
   for (i = 1; i < 16; i++) {
     for (e = 1; e < 16; e++) {
       $("#line" + i).find("#space" + e).html("");
+    }
   }
 }
-}
 
-
+//refresh your letters button
 $("#generate").click(function () {
   var i; //index variable
-  clearLine();
+  clearBoard(); 
+  for (i = 1; i <= 7; i++)//for loop to throw current letter in bag
+  {
+    var letter = $("#tile" + i).find(".game_tile").attr("letter");
+    ScrabbleTiles[letter]["number-remaining"]++;
+    remainingTiles++;
+    $("#tile" + i).html("");
+  }
   for (i = 0; i < 7; i++)//for loop to fill out letter holder with new letters
   {
-    $("#tile" + (i + 1)).html("");
     refreshTile(i);
   }
-  $("#tileCount").html("Tiles Left: " + remainingTiles)
 });
 
+// submit current word button
 $("#save").click(function () {
+  // if current word is not valid then open dialog box
   if (currentWord == "") {
-    $("#currentScore").html("Invalid word! Please try Again");
+    $( "#message" ).dialog( "open" );
     return;
   }
-  var i; //index variable
-  currentScore += currentWordScore;
-  clearLine();
-  for (i = 0; i < 7; i++)//for loop to fill out letter holder with new letters
+  var i; // index variable
+  currentScore += currentWordScore; // add word score
+  clearBoard(); // clear board of tiles
+  for (i = 0; i < 7; i++)// for loop to fill out letter holder with new letters
   {
     refreshTile(i);
   }
+
+  // update all our friendly output 
   $("#currentScore").html("Score: " + currentScore)
   $("#tileCount").html("Tiles Left: " + remainingTiles)
   $("#currentWord").html("Word: N/a");
   $("#currentWord2").html("Word Score: 0");
+
+  // reset current word values
+  currentWordScore = 0;
+  currentWord = "";
 });
 
+// dialog box to alert player if word is not valid
+$( function() {
+  $( "#message" ).dialog({
+    modal: true,
+    autoOpen: false,
+    buttons: {
+      Ok: function() {
+        $( this ).dialog( "close" );
+      }
+    }
+  });
+});
 
 $(document).ready(function () {
-  var i; //index variable
-  for (i = 0; i < 7; i++)//for loop to fill out letter holder with new letters
+  var i; // index variable
+  for (i = 0; i < 7; i++)// for loop to fill out letter holder with new letters
   {
     refreshTile(i);
   }
